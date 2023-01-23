c6a7aa91da5374a0e98ddeec8015d8acd384be4a
Ali Görkem Aksöz <a.gorkemaksoz@gmail.com>
2023-01-23 18:18:53 +0300

    Initial Commit

---
+++ Foodilicious/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Foodilicious
+//
+//  Created by Ali Görkem Aksöz on 14.01.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
