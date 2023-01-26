db207ed8aab110c63232c8bd656710ebacb6964c
Ali Görkem Aksöz <a.gorkemaksoz@gmail.com>
2023-01-26 16:14:54 +0300

    Installed kingfisher

---
+++ Foodilicious/CustomViews/CollectionViewCells/CategoryCollectionViewCell/CategoryCollectionViewCell.swift
@@ -0,0 +1,20 @@
+//
+//  CategoryCollectionViewCell.swift
+//  Foodilicious
+//
+//  Created by Ali Görkem Aksöz on 26.01.2023.
+//
+
+import UIKit
+
+class CategoryCollectionViewCell: UICollectionViewCell {
+
+    
+    @IBOutlet weak var categoryImageView: UIImageView!
+    @IBOutlet weak var categoryTitleLabel: UILabel!
+    
+    func setup(category: DishCategory) {
+        
+    }
+
+}
---
+++ Foodilicious/Models/DishCategory.swift
@@ -0,0 +1,13 @@
+//
+//  DishCategory.swift
+//  Foodilicious
+//
+//  Created by Ali Görkem Aksöz on 26.01.2023.
+//
+
+import Foundation
+
+
+struct DishCategory {
+    let id, name, image: String
+}
