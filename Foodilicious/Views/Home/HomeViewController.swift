eb5872b63ad9168eaaca7c94b4039de5ba5c4f61
Ali Görkem Aksöz <a.gorkemaksoz@gmail.com>
2023-01-26 13:25:46 +0300

    Homescreen's template has done

---
+++ Foodilicious/Views/Home/HomeViewController.swift
@@ -0,0 +1,17 @@
+//
+//  HomeViewController.swift
+//  Foodilicious
+//
+//  Created by Ali Görkem Aksöz on 24.01.2023.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+}
